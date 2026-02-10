07eae22f3b242391ba2a45d2f7930b760d58222c
CristinaJung <cristijung@gmail.com>
2026-02-10 11:30:50 -0300

    correção e ajuste de nome

--- app/artistas/page.tsx
+++ app/artistas/page.tsx
@@ -1,12 +1,14 @@
-import ArtistList from "@/components/artists/ArtistsList";
+import ArtistList from "@/components/artistsList/ArtistsList";
 
 
 export default function Artistas() {
     return(
-    <>
-    <h1>Artistas</h1>
-    <ArtistList />
-    </>
-    
-);
+        <>
+        <h1>Artistas do PopRock</h1>
+        <hr/>
+        <ArtistList />
+        
+        </>
+    );
+
 }
